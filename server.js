3363091cab726ae3e68ba85ce0683ffc52fee4b9
Ajao Abdur-Raqeeb <Ajaorqb@gmail.com>
2020-06-22 10:38:57 +0100

    Finished API and created a react app

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the Contact Keeper API...' })
 );
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 // Define Routes
 
-app.use('/api/users', require('./Routes/users'));
-app.use('/api/contacts', require('./Routes/contacts'));
-app.use('/api/auth', require('./Routes/auth'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 
